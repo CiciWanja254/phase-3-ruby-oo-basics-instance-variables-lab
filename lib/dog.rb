722e9b635e4cebfa7d13ddb1dd6c5f2a748b9b29
Cici254 <thukucindy00@gmail.com>
2023-05-18 20:55:47 +0300

    Complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
+  # setter method
+  def name=(value)
+    @this_dogs_name = value
+  end
 
+  # getter method
+  def name
+    # access the data from the @this_dogs_name instance variable and return it
+    @this_dogs_name
+  end
 end
